30bd39989839b6c1dc122ed4c4c9d700185e0298
Your Name <victor_bankowski@hotmail.com>
2018-09-12 01:01:37 +0300

    moved the initialisation of the stop graph to it's own function. started implementing the actual search functionality

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,35 @@ use std::fs::File;
 use std::io::Read;
 use std::io::BufReader;
 use std::collections::HashMap;
+use std::collections::VecDeque;
 
 fn main() {
-    println!("Hello, world!");
+    let stops = load_stops();
+    let visited = HashMap::new();
+    let queue = VecDeque::new();
+    println!("From:");
+    let mut from = String::new();
+    io::stdin().read_line(&mut from).expect("Failed to read line");
+    println!("To:");
+    let mut to = String::new();
+    io::stdin().read_line(&mut to).expect("Failed to read line");
+    queue.push_front();
+}
+
+fn load_stops() -> HashMap<String, Stop> {
     let file_path = Path::new("./resource/network.json");
     let file = File::open(file_path).unwrap();
+
     let mut data = String::new();
     let mut buf_reader = BufReader::new(file);
     buf_reader.read_to_string(&mut data).unwrap();
+
     let stops: Vec<Stop> = serde_json::from_str(&data).unwrap();
     let mut stop_map = HashMap::new();
     for stop in stops {
         stop_map.insert(stop.code.clone(), stop);
-    }
-    println!("{:?}", stop_map)
+    };
+    stop_map
 }
 
 #[derive(Debug, Serialize, Deserialize)]
