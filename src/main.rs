fe3372629836223d9bad638cc24d83eb0f241b23
Your Name <victor_bankowski@hotmail.com>
2018-09-12 15:01:52 +0300

    almost done. some errors still but I gotta go to the exercise session

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,57 @@ extern crate serde_json;
 
 use std::path::Path;
 use std::fs::File;
+use std::io;
 use std::io::Read;
 use std::io::BufReader;
 use std::collections::HashMap;
 use std::collections::VecDeque;
 
+
 fn main() {
     let stops = load_stops();
     let visited = HashMap::new();
-    let queue = VecDeque::new();
+    let mut queue = VecDeque::new();
     println!("From:");
     let mut from = String::new();
     io::stdin().read_line(&mut from).expect("Failed to read line");
     println!("To:");
     let mut to = String::new();
     io::stdin().read_line(&mut to).expect("Failed to read line");
-    queue.push_front();
+    queue.push_front((from, from));
+
+    let mut visited = HashMap::new();
+    while let Some((parent, stop)) = queue.pop_back() {
+        if stop == to {
+            trace_back_and_print(stop, parent, visited, stops);
+            break;
+        } else {
+            if visited.contains_key(stop) {
+                continue;
+            }
+            visited.insert(stop, parent);
+            let stop = stops.get(&stop).unwrap();
+            for neighbour in stop.neighbors.keys() {
+                queue.push_front((stop.code, *neighbour));
+            }
+        }
+    }
+}
+
+fn trace_back_and_print(destination: String, parent: String, visited: HashMap<String, String>, stops: HashMap<String, Stop>) {
+    let stop = stops.get(&destination).unwrap();
+    let mut parent = stops.get(&parent).unwrap();
+    let mut route = String::new();
+    route.push_str(&format!("{}({})[DESTINATION]", stop.code, stop.address));
+    route.push_str(" -> ");
+    route.push_str(&format!("{}({})", parent.code, parent.address));
+    while let Some(parent_id) = visited.get(&parent.code) {
+        route.push_str(" -> ");
+        let parent = stops.get(&parent_id).unwrap();
+        route.push_str(&format!("{}({})", parent.code, parent.address));
+    }
+    route.push_str("[START]");
+    println!("{}", route);
 }
 
 fn load_stops() -> HashMap<String, Stop> {
